810c0b6afb5bd7bb89b9d405fe8b74760abbc451
Ivan Cavero <ivangonzalezcavero@gmail.com>
2022-04-17 00:30:33 +0200

    :truck: Wrong patch.
    Wrong patch.

--- components/installFonts.ps1
+++ components/installFonts.ps1
@@ -1,12 +1,10 @@
 # Declare variables
-
 $repoName = "ryanoasis/nerd-fonts"
 $assetPattern = "Hack.zip"
-$extractDirectory = "./tmp"
-$fontsFolder = "./tmp/fonts"
+$extractDirectory = "../tmp"
+$fontsFolder = "../tmp/fonts"
 
 # Get latest release of the repo and download the asset with the pattern Hack.zip
-
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
 $asset = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $assetPattern
 $downloadUri = $asset.browser_download_url
@@ -15,11 +13,9 @@ $extractPath = [System.IO.Path]::Combine($extractDirectory, $asset.name)
 Invoke-WebRequest -Uri $downloadUri -Out $extractPath
 
 # Extract the zip file
-
-Expand-Archive -path "./tmp/$assetPattern" -destinationpath $fontsFolder -Force
+Expand-Archive -path "../tmp/$assetPattern" -destinationpath $fontsFolder -Force
 
 # Install the fonts
-
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 foreach ($file in gci $fontsFolder/*.ttf)
 {
