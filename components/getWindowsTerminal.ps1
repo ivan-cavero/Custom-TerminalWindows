60bb0ef9157a384e34d56943b9afe19e20fa0650
Ivan Cavero <ivangonzalezcavero@gmail.com>
2022-04-17 00:27:19 +0200

    :sparkles: Detect if use W10 or W11 automatically.
    Detect if use W10 or W11 automatically.

--- components/getWindowsTerminal.ps1
+++ components/getWindowsTerminal.ps1
@@ -1,24 +1,15 @@
 # Declare variables
-
+$getWindowsEdition = ((Get-WMIObject win32_operatingsystem).name -split ' ')[2]
 $repoName = "microsoft/terminal"
-$assetPattern = "Microsoft.WindowsTerminal_Win11*.msixbundle"
+$assetPattern = "Microsoft.WindowsTerminal_Win$getWindowsEdition*.msixbundle"
 $extractDirectory = "../tmp"
 
 # Get latest release of the repo and download the asset with the pattern Microsoft.WindowsTerminal_Win11*.msixbundle
-
 $releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
 $asset = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $assetPattern
 $downloadUri = $asset.browser_download_url
 
 $extractPath = [System.IO.Path]::Combine($extractDirectory, $asset.name)
-Invoke-WebRequest -Uri $downloadUri -Out $extractPath
+Invoke-WebRequest -Uri $downloadUri -OutFile $extractPath
 
-Add-AppPackage -path "../tmp/Microsoft.WindowsTerminal_Win11*.msixbundle"
-
-# Set Windows Terminal as default
-
-$configLocation = "C:\Users\ivang\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-$config = Get-Content $configLocation
-$config.defaultApp = "Microsoft.WindowsTerminal_8wekyb3d8bbwe"
-Set-Content $configLocation $config
-
+Add-AppPackage -path "../tmp/Microsoft.WindowsTerminal_Win$getWindowsEdition*.msixbundle"
