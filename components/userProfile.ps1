d4af5d83eb6c9f601193291fdfedc60d6a98b4ad
Ivan Cavero <ivangonzalezcavero@gmail.com>
2022-04-16 23:33:53 +0200

    :zap: Add path2 variable.
    Add path2 variable.

--- components/userProfile.ps1
+++ components/userProfile.ps1
@@ -1,6 +1,12 @@
 $username=( ( Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty username ) -split '\\' )[1]
-$path = "C:\Users\$username"
+$path1 = "C:\Users\$username\.config\powershell"
 
-mkdir -p $path\.config\powershell
+mkdir -p $path1
 
-cp -r ..\config\user_profile.ps1 $path\.config\powershell
+cp -r ..\config\user_profile.ps1 $path1
+
+cp -r ..\config\ivan.omp.json $path1
+
+$path2 = "C:\Users\$username\Documents\PowerShell"
+
+cp -r ..\config\Microsoft.PowerShell_profile.ps1 $path2
