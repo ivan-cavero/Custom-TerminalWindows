5a88f46b599a279ef3c14004a7c704b523c88eb3
Ivan Cavero <ivangonzalezcavero@gmail.com>
2022-04-16 23:32:11 +0200

    :rocket: First Test
    First Test

--- customTerminal.ps1
+++ customTerminal.ps1
@@ -3,8 +3,21 @@ if ('S-1-5-32-544' -notin [System.Security.Principal.WindowsIdentity]::GetCurren
     throw 'Script must run as admin!'
 }
 
+# Import installFonts
+$ScriptRunInstallFonts= $PSScriptRoot+"\components\installFonts.ps1"
+&$ScriptRunInstallFonts
+
+# Import getWindowsTerminal
+$ScriptRunWindowsTerminal= $PSScriptRoot+"\components\getWindowsTerminal.ps1"
+&$ScriptRunWindowsTerminal
+
+# Import getPowerShell
+$ScriptRunPowerShell= $PSScriptRoot+"\components\getPowerShell.ps1"
+&$ScriptRunPowerShell
+
 # Install scoop
-iwr -useb get.scoop.sh | iex
+iwr -useb get.scoop.sh -outfile '.\tmp\scoop.ps1'
+.\tmp\scoop.ps1 -RunAsAdmin
 
 # Install curl
 scoop install curl sudo jq
@@ -14,3 +27,27 @@ winget install -e --id Git.Git
 
 # Install NeoVim
 scoop install neovim gcc
+
+# Import userProfile
+$ScriptRunUserProfile= $PSScriptRoot+"\components\userProfile.ps1"
+&$ScriptRunUserProfile
+
+# Install Oh My Posh
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Module oh-my-posh -Scope CurrentUser -Force
+
+# Install nvm
+scoop install nvm
+
+# Install Terminal Icons
+Install-Module -Name TerminalIcons -Repository PSGallery -Force
+
+# Install Z Shell
+Install-Module -Name z -Force
+
+# Install PSReadLine - AutoCompletion
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+
+# Install fzf
+scoop install fzf
+Install-Module -Name PSFzf -Scope CurrentUser -Force
