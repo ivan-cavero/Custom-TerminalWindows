53c77badd66941f8d8493c38f4a0253b34e687af
Ivan Cavero <ivangonzalezcavero@gmail.com>
2022-04-18 20:41:13 +0200

    :zap: New install with choco.
    Is more faster than install with msi.

--- components/getPowerShell.ps1
+++ components/getPowerShell.ps1
@@ -1,14 +1,3 @@
-# Declare variables
-$repoName = "PowerShell/PowerShell"
-$assetPattern = "PowerShell*x64.msi"
-$extractDirectory = "./tmp"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Get latest release of the repo and download the asset with the pattern Microsoft.WindowsTerminal_Win11*.msixbundle
-$releasesUri = "https://api.github.com/repos/$repoName/releases/latest"
-$asset = (Invoke-WebRequest $releasesUri | ConvertFrom-Json).assets | Where-Object name -like $assetPattern
-$downloadUri = $asset.browser_download_url
-
-$extractPath = [System.IO.Path]::Combine($extractDirectory, $asset.name)
-Invoke-WebRequest -Uri $downloadUri -Out $extractPath
-
-Start-Process msiexec.exe -Wait -ArgumentList '/I ./tmp/$assetPattern /quiet'
+choco install -y powershell-core
